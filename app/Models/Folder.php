9bf874014f8f147b5a614b19cb89b37a4218fd25
Adil Abdu Bushra <adilabdu68@gmail.com>
2022-01-20 18:00:05 +0900

    New function to scope Folder object return by its user_id

--- app/Models/Folder.php
+++ app/Models/Folder.php
@@ -4,26 +4,43 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @method static where(array $array)
+ * @method static create(\Illuminate\Http\Request[] $array)
+ *
+ * @property mixed $children
+ */
 class Folder extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'parent_id'];
+    protected $fillable = ['name', 'parent_id', 'user_id'];
 
-    public function user() {
+    public function user(): BelongsTo
+    {
         return $this->belongsTo(User::class);
     }
 
-    public function parent() {
+    public function parent(): BelongsTo
+    {
         return $this->belongsTo(Folder::class, 'parent_id');
     }
 
-    public function children() {
+    public function children(): HasMany
+    {
         return $this->hasMany(Folder::class, 'parent_id');
     }
 
-    public function files() {
+    public function files(): HasMany
+    {
         return $this->hasMany(File::class);
+    }
+
+    public function scopeOwner($query, $user_id)
+    {
+        return $query->where('user_id', $user_id);
     }
 }
